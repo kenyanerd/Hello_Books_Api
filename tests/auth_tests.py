24fde2e3f2a70549d7430fe497ff95eb5d8de53c
kenyanerd <mwemadanson@outlook.com>
2018-04-11 21:05:26 +0300

    fix bug in test

--- tests/auth_tests.py
+++ tests/auth_tests.py
@@ -1,12 +1,4 @@
 import unittest
-<<<<<<< HEAD
-
-import json
-
-from app import create_app
-
-class UserAuthTestCase(Unittest.TestCase):
-=======
 #import sys
 
 import json
@@ -14,44 +6,24 @@ import json
 #sys.path.append('..')
 from Hello_Books_Api.app import create_app
 
+
 class UserAuthTestCase(unittest.TestCase):
->>>>>>> api_endpoints
     '''User Authentication TestCase'''
 
     def setUp(self):
         """Set up test variables. """
 
-<<<<<<< HEAD
-        self.app = create_app("testing")
-
-        '''initialize the test client'''
-        self.client = self.app.test_client
-=======
         self.app = create_app()
 
         '''initialize the test client'''
         self.client = self.app.test_client()
->>>>>>> api_endpoints
 
         '''This is the user test json data with a predefined email and password'''
         self.user_data = {
             'email': 'user@testmail.com',
-            'password': 'pass123*'
+            'password': 'Pass123*'
 
         }
-<<<<<<< HEAD
-        def tearDown(self):
-            self.app_context.pop()
-
-    def test_user_registration(self):
-        '''test user registration works correctly'''
-
-
-        res = self.client.post('/auth/register', data=self.user_data)
-
-        '''get the results returned in json format'''
-        result = json.loads(res.data.decode()
-=======
 
     def test_user_registration(self):
         '''test user registration works correctly'''
@@ -59,7 +31,6 @@ class UserAuthTestCase(unittest.TestCase):
 
         '''get the results returned in json format'''
         result = json.loads(res.data.decode())
->>>>>>> api_endpoints
 
         '''assert that the request contains a success message and a 201 status code'''
         self.assertEqual(result['message'], "Registration successful.")
@@ -71,14 +42,10 @@ class UserAuthTestCase(unittest.TestCase):
         res = self.client.post('/auth/register', data=self.user_data)
         self.assertEqual(res.status_code, 201)
         second_res = self.client.post('/auth/register', data=self.user_data)
-<<<<<<< HEAD
-        self.assertEqual(second_res.status_code, 202)
-=======
         self.assertEqual(second_res.status_code, 409)
->>>>>>> api_endpoints
         result = json.loads(second_res.data.decode())
-        self.assertEqual(result['message'], "This  account already  Exists. Please login.")
-
+        self.assertEqual(result['message'],
+                         "This  account already  Exists. Please login.")
 
     def test_user_login(self):
         '''test user can login successfully'''
@@ -91,37 +58,23 @@ class UserAuthTestCase(unittest.TestCase):
         self.assertEqual(login_res.status_code, 200)
         self.assertTrue(result['access_token'])
 
-
     def test_user_logout(self):
         ''' test if a user can log-out'''
-<<<<<<< HEAD
 
-    	self.test_user_login
-	    res = self.client.get('/auth/logout', data =self.user_data)
-=======
         self.test_user_login()
-        res = self.client.get('/auth/logout', data =self.user_data)
->>>>>>> api_endpoints
+        res = self.client.get('/auth/logout', data=self.user_data)
         self.assertIn(b'you logged out', res.data)
 
-
     def test_user_password_reset(self):
-<<<<<<< HEAD
-    """test app if app can allow user to reset their password """
+        """test app if app can allow user to reset their password """
 
         self.test_user_login()
-        reset_result=self.client.post('/auth/password_reset', data = {
-            'email': 'user@testmail.com,
-=======
-        """test app if app can allow user to reset their password """
-        self.test_user_login()
-        res =self.client.post('/auth/password_reset', data = {
+        res = self.client.post('/auth/password_reset', data={
             'email': 'user@testmail.com',
->>>>>>> api_endpoints
             'old_password': 'pass123*',
             'new_password': 'PAss123*'
-         })
-        login_result=self.client.post('/auth/login', data={
+        })
+        login_result = self.client.post('/auth/login', data={
             'email': 'user@testmail.com',
             'password': 'PAss123*'
         })
--- tests/crud_tests.py
+++ tests/crud_tests.py
@@ -2,77 +2,52 @@
 # path ../tests/book_api_tests.py
 
 # import os and python unittest modules
-<<<<<<< HEAD
-
-=======
 #import sys
->>>>>>> api_endpoints
 import unittest
 
 # import json
 import json
 
-<<<<<<< HEAD
-from app import create_app
-=======
 #sys.path.append('..')
 from Hello_Books_Api.app import create_app
->>>>>>> api_endpoints
-
-
 
 
 class HellobooksCRUDTestCase(unittest.TestCase):
     '''This class represents the Hello_Books_Api CRUD Testcase'''
 
     def setUp(self):
-
         '''Define test variables and initialize app.'''
         self.app = create_app('testing')
         self.app_context = self.app.app_context()
         self.app_context.push()
-<<<<<<< HEAD
-
-=======
         self.client = self.app.test_client()
->>>>>>> api_endpoints
+
         # book(dict) variable
         self.book_details = {
             'id': 4,
-            'title':'Eloquent Javascript',
+            'title': 'Eloquent Javascript',
             'edition': 'Second',
             'description': 'a javascript book',
             'author': 'Marijn Haverbeke',
-            'category':'Javascript',
+            'category': 'Javascript',
             'copies': 20
 
         }
 
-        #intialize the test client
-<<<<<<< HEAD
-        self.client = self.app.test_client
-=======
-
->>>>>>> api_endpoints
-
     def tearDown(self):
         self.app_context.pop()
-
 
     def test_book_info_can_be_modified(self):
         '''test whether book info can be updated'''
 
-        result_post=self.client.post('/app/books/', data = self.book_details)
+        result_post = self.client.post('/app/books/', data=self.book_details)
         self.assertEqual(result_post.status_code, 201)
         new_book_detail = self.book_details['description'] = "A modern introduction to programming"
-        result_put = self.client.put('/app/books/4', data = new_book_detail)
-<<<<<<< HEAD
-        self.assertEqual(response_put.status_code, 200)
-=======
+        result_put = self.client.put('/app/books/4', data=new_book_detail)
         self.assertEqual(result_put.status_code, 200)
->>>>>>> api_endpoints
         result_get = self.client.get('/app/books/4')
-        self.assertIn('A modern introduction to programming',str(result_get.data))
+        self.assertIn('A modern introduction to programming',
+                      str(result_get.data))
 
     def test_api_can_remove_book(self):
         '''test api can delete a book'''
@@ -82,67 +57,34 @@ class HellobooksCRUDTestCase(unittest.TestCase):
         result_delete = self.client.delete('app/books/4')
         self.assertEqual(result_delete.status_code, 200)
         result = self.client.get('/app/books/4')
-<<<<<<< HEAD
-      	self.assertEqual(result.status_code, 404)
-=======
         self.assertEqual(result.status_code, 404)
->>>>>>> api_endpoints
 
     def test_get_book_by_id(self):
         '''test api can get a single book by its id '''
 
-        result = self.client.post('/app/books/', data = self.book_details)
+        result = self.client.post('/app/books/', data=self.book_details)
         self.assertEqual(result.status_code, 201)
-        json_result= json.loads(result.data.decode('utf-8'))
+        json_result = json.loads(result.data.decode('utf-8'))
 
-<<<<<<< HEAD
-        result_get = self.client.get(
-                        '/app/books/{}'.format(json_result['id']
-                         ))
-=======
         result_get = self.client.get('/app/books/{}'.format(json_result['id']))
->>>>>>> api_endpoints
         self.assertEqual(result_get.status_code, 200)
         self.assertIn('Eloquent Javascript', result_get.data)
 
     def test_api_can_add_book(self):
         '''test api can borrow book'''
 
-        result = self.client.post('/app/books/', data = self.book_details)
-<<<<<<< HEAD
-        self.assertEqual(response.status_code, 201)
-        self.assertIn('Marijn Haverbeke', str(result.data)
-
-
-    def test_api_can_remove_book(self):
-        '''test api can delete a book'''
-
-        result_post = self.client.post('/app/books/', data = self.book_details)
-        self.assertEqual(result_post.status_code, 201)
-        result_delete = self.client.delete('app/books/4')
-        self.assertEqual(result_delete.status_code, 200)
-        result = self.client.get('/app/books/4')
-      	self.assertEqual(result.status_code, 404)
-=======
+        result = self.client.post('/app/books/', data=self.book_details)
         self.assertEqual(result.status_code, 201)
         self.assertIn('Marijn Haverbeke', str(result.data))
->>>>>>> api_endpoints
-
 
     def test_api_can_get_all_books(self):
         '''test api can get all books'''
 
-        result_post = self.client.post('/app/books/', data = self.book_details)
+        result_post = self.client.post('/app/books/', data=self.book_details)
         self.assertEqual(result_post.status_code, 201)
         result_get = self.client.get('/app/books/')
         self.assertEqual(result_get.status_code, 200)
-<<<<<<< HEAD
-        self.assertIn('Eloquent Javascript', str(result_get)
-
-
-=======
         self.assertIn('Eloquent Javascript', str(result_get))
->>>>>>> api_endpoints
 
 
 if __name__ == "__main__":
